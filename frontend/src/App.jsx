c687cf1fbfb1ff63dff8ef0d6ac8661a38d1d474
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-16 12:45:48 -0700

    changed photos for main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import PhotoList from './components/PhotoList';
 import './App.scss';
+import './styles/PhotoListItem.scss';
+
 
 const App = () => {
-  const photos = [1]; // Create an array of three elements
-
+  const photos = [1, 2, 3]; // Create an array of three elements
+  console.log("test", photos);
   return (
     <div className="App">
       <div className='photo-list'>
       {photos.map((photoId) => (
-        <PhotoList key={photoId} />
+        <PhotoListItem key={photoId} />
         ))}
       </div>
     </div>
